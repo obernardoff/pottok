b58661863ddb30e163ac08702049e5ac49f2d39c
oliviab <olivia.bernardoff@outlook.fr>
2020-06-23 11:07:52 +0200

    examples pottok with labels

--- examples/plot_transform_sinkhorn_labels_pottoks.py
+++ examples/plot_transform_sinkhorn_labels_pottoks.py
@@ -30,23 +30,60 @@ black_pottok = black_pottok/255
 
 
 ##############################################################################
-# Optimal transport with SinkhornL1l2 with gridsearch
-# ----------------------------------------------------
+# Optimal transport with SinkhornL1l2 with circular gridsearch
+# --------------------------------------------------------------
 
-gridsearch_transport = pottok.OptimalTransportGridSearch(transport_function=ot.da.SinkhornL1l2Transport,
+gridsearch_transport_circular = pottok.OptimalTransportGridSearch(transport_function=ot.da.SinkhornL1l2Transport,
                                         params=dict(reg_e=[1e-1,1e-0], reg_cl=[1e-1,1e-0]))
-gridsearch_transport.preprocessing(Xs=Xs,ys=ys,Xt=Xt,yt=yt,scaler=False)
-gridsearch_transport.fit_circular()
-# gridsearch_transport.fit_crossed()
+gridsearch_transport_circular.preprocessing(Xs=Xs,ys=ys,Xt=Xt,yt=yt,scaler=False)
+gridsearch_transport_circular.fit_circular()
 
+
+# Best grid is {'reg_e': 1.0, 'reg_cl': 0.1}
 
 ##############################################################################
 # Plot images
 # -----------------------
 
-brown_pottok_transp = gridsearch_transport.predict_transfer(brown_pottok.reshape(-1,3))
+brown_pottok_transp_circular = gridsearch_transport_circular.predict_transfer(brown_pottok.reshape(-1,3))
+pl.figure(1, figsize=(10,8))
+
+pl.subplot(2, 2, 1)
+pl.imshow(brown_pottok)
+pl.axis('off')
+pl.title('Brown pottok (Source)')
+
+pl.subplot(2, 2, 3)
+pl.imshow(black_pottok)
+pl.axis('off')
+pl.title('Black pottok (Target)')
+
+pl.subplot(2, 2, 4)
+pl.imshow(brown_pottok_transp_circular.reshape(*brown_pottok.shape))
+pl.axis('off')
+pl.title('SinkhornL1l2 (Source to Target with labels)')
+
+pl.show()
 
 
+
+
+##############################################################################
+# Optimal transport with SinkhornL1l2 with crossed gridsearch
+# --------------------------------------------------------------
+
+gridsearch_transport_crossed = pottok.OptimalTransportGridSearch(transport_function=ot.da.SinkhornL1l2Transport,
+                                        params=dict(reg_e=[1e-1,1e-0], reg_cl=[1e-1,1e-0]))
+gridsearch_transport_crossed.preprocessing(Xs=Xs,ys=ys,Xt=Xt,yt=yt,scaler=False)
+gridsearch_transport_crossed.fit_crossed()
+
+# Best grid is {'reg_e': 0.1, 'reg_cl': 0.1}
+
+##############################################################################
+# Plot images
+# -----------------------
+
+brown_pottok_transp_crossed = gridsearch_transport_crossed.predict_transfer(brown_pottok.reshape(-1,3))
 pl.figure(2, figsize=(10,8))
 
 pl.subplot(2, 2, 1)
@@ -60,39 +97,36 @@ pl.axis('off')
 pl.title('Black pottok (Target)')
 
 pl.subplot(2, 2, 4)
-pl.imshow(brown_pottok_transp.reshape(*brown_pottok.shape))
+pl.imshow(brown_pottok_transp_crossed.reshape(*brown_pottok.shape))
 pl.axis('off')
 pl.title('SinkhornL1l2 (Source to Target with labels)')
 
 pl.show()
 
-###########
+
+##############################################################################
+# Comparison with pot
+# ----------------------------------
 
 
-
-ot_mapping_linear = ot.da.SinkhornL1l2Transport(
+ot_mapping_linear_circular = ot.da.SinkhornL1l2Transport(
     reg_e=1.0, reg_cl=0.1,verbose=True)
-ot_mapping_linear.fit(Xs=Xs, ys = ys, Xt=Xt, yt = yt)
+ot_mapping_linear_circular.fit(Xs=Xs, ys = ys, Xt=Xt, yt = yt)
+brown_pottok_transp_pot_circular = ot_mapping_linear_circular.transform(brown_pottok.reshape(-1,3))
 
-
-brown_pottok_transp_pot= ot_mapping_linear.transform(brown_pottok.reshape(-1,3))
-
-pl.figure(2, figsize=(10,8))
-
-pl.subplot(2, 2, 1)
-pl.imshow(brown_pottok)
-pl.axis('off')
-pl.title('Brown pottok (Source)')
-
-pl.subplot(2, 2, 3)
-pl.imshow(black_pottok)
-pl.axis('off')
-pl.title('Black pottok (Target)')
-
-pl.subplot(2, 2, 4)
-pl.imshow(brown_pottok_transp_pot.reshape(*brown_pottok.shape))
-pl.axis('off')
-pl.title('SinkhornL1l2 (Source to Target with labels)')
-
-pl.show()
+if (brown_pottok_transp_pot_circular == brown_pottok_transp_circular).all() : 
+    print ("POT and Pottok give same transformation - circular")
+else : 
+    print ("ERROR : POT and Pottok do not give same transformation")
+   
+ot_mapping_linear_crossed = ot.da.SinkhornL1l2Transport(
+    reg_e=0.1, reg_cl=0.1,verbose=True)
+ot_mapping_linear_crossed.fit(Xs=Xs, ys = ys, Xt=Xt, yt = yt)
+brown_pottok_transp_pot_crossed = ot_mapping_linear_crossed.transform(brown_pottok.reshape(-1,3))
+   
+   
+if (brown_pottok_transp_pot_crossed == brown_pottok_transp_crossed).all() : 
+    print ("POT and Pottok give same transformation - crossed")
+else : 
+    print ("ERROR : POT and Pottok do not give same transformation")
 
